63017570389a95f84057c0301cb9ff99745ebc35
SAurav <sauravavishek@gmail.com>
2023-06-22 21:32:12 +0530

    Time: 60 ms (50.09%), Space: 41.8 MB (85.40%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -4,8 +4,6 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function(word1, word2) {
-    const wordToString1 = word1.join("")
-    const wordToString2 = word2.join("")
-    if(wordToString1.length !== wordToString2.length) return false
-    return wordToString1 === wordToString2
+        return word1.join("") === word2.join("")
+
 };
