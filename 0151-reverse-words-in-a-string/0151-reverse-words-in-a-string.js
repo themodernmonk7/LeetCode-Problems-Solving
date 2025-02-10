b383e2563a309dc05fff33e70132e83f69a58e99
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-10 21:23:16 +0530

    Time: 2 ms (66.36%), Space: 51.4 MB (22.22%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,6 +3,24 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-        return s.replace(/\s+/g, ' ').split(' ').reverse().join(" ").trim(' ')
+let result = "";
+  let word = "";
+  let i = s.length - 1;
 
+  while (i >= 0) {
+    if (s[i] !== " ") {
+      word = s[i] + word; // Build the current word
+    } else if (word.length > 0) {
+      result += (result ? " " : "") + word; // Add the word to the result
+      word = ""; // Reset the word
+    }
+    i--;
+  }
+
+  // Add the last word if there is any
+  if (word.length > 0) {
+    result += (result ? " " : "") + word;
+  }
+
+  return result;
 };
