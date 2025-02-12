730f069bf0771f39b19338b96e27184c80292317
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-12 22:13:20 +0530

    Time: 81 ms (74.57%), Space: 62.2 MB (26.72%) - LeetHub

--- 2804-compact-object/2804-compact-object.js
+++ 2804-compact-object/2804-compact-object.js
@@ -4,15 +4,15 @@
  */
 var compactObject = function(obj) {
     if(typeof obj !== 'object' || obj === null) return obj
-  if(Array.isArray(obj)) {
-    return obj.reduce((accumulator, value) => {
-      if(value) accumulator.push(compactObject(value))
-      return accumulator
-    }, [])
-  }
-  
-  return Object.keys(obj).reduce((accumulator, key) => {
-    if(obj[key]) accumulator[key] = compactObject(obj[key])
-    return accumulator
-  }, {})
+    if(Array.isArray(obj)) {
+        return obj.reduce((accumulator, key) => {
+            if(key) accumulator.push(compactObject(key))
+            return accumulator
+        }, [])
+    }
+
+    return Object.keys(obj).reduce((accumulator, key) => {
+        if(obj[key]) accumulator[key] = compactObject(obj[key])
+        return accumulator
+    }, {})
 };
