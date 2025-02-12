c33a859fbd7f499b7dd41a9273f326227b36ff8c
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-12 21:56:01 +0530

    Time: 176 ms (19.98%), Space: 81.2 MB (28.55%) - LeetHub

---
+++ 2759-flatten-deeply-nested-array/2759-flatten-deeply-nested-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    const flatten = (array, depth) => {
+        if(depth >= n) return array
+
+        let result = []
+
+        for(let element of array) {
+            if(Array.isArray(element)) {
+                result.push(...flatten(element, depth + 1))
+            } else {
+                result.push(element)
+            }
+        }
+        return result
+        
+    }
+
+    return flatten(arr, 0)
+};
