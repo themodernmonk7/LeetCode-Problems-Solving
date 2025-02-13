793378f413dc00b54b09b8c892df30af41e6c062
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-13 08:30:59 +0530

    Time: 12 ms (79.74%), Space: 54 MB (39.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,7 +4,17 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-//     Check if the string length is same or not
-    if(s.lenght !== t.lenght) return false
-    return s.split('').sort().join("") === t.split('').sort().join("")
+    if(s.length !== t.length) return false
+
+    let charFreq = {}
+
+    for(let char of s) {
+        charFreq[char] = charFreq[char] ? charFreq[char] + 1 : 1
+    }
+
+    for(let char of t) {
+        if(!charFreq[char]) return false
+        charFreq[char]--
+    }
+    return true
 };
