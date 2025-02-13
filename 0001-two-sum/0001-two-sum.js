a5cd9633d2e5b727e8b15af2db6a73e1c4771312
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-13 12:21:00 +0530

    Time: 4 ms (60.95%), Space: 52 MB (11.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  let map = new Map()
+    let map = new Map()
+    const n = nums.length
 
-  for(let i = 0; i < nums.length; i++) {
-    let firstNum  = nums[i]
-    let secondNum = target - firstNum
+    for(let i = 0; i < n; i++) {
+        let firstNum = nums[i]
+        let secondNum = target - firstNum
 
-    if(map.has(secondNum)) {
-        return [map.get(secondNum),i ]
+        if(map.has(secondNum)) {
+            return [map.get(secondNum), i]
+        }
+        map.set(nums[i], i)
     }
-    map.set(nums[i], i)
-  } 
-  return [] 
 };
