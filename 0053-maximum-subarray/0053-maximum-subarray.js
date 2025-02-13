9362705544deba7a4a90c1e51a9396fd0e45769d
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-13 14:45:29 +0530

    Time: 1 ms (85.81%), Space: 57.8 MB (79.91%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    const n = nums.length
+    let maxSum = -Infinity
+    let currentSum = 0
+
+    for(let i = 0; i < n; i++) {
+        currentSum += nums[i]
+        maxSum = Math.max(maxSum, currentSum)
+
+        if(currentSum <= 0) currentSum = 0
+    }
+
+return maxSum    
+};
