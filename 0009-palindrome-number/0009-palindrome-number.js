1a633cbeb29d1b3489e5e834526b0656a4f0ca7a
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-13 08:38:25 +0530

    Time: 4 ms (96.52%), Space: 58.2 MB (79.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,6 +3,15 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    return x ===  Number([x].join("").split('').reverse().join(''))
+    const arrayNum = x.toString().split("")
+    const n = arrayNum.length
+    let left = 0
+    let right = n - 1
 
+    while(left <= right) {
+        if(arrayNum[left] !== arrayNum[right]) return false
+        left++
+        right--
+    }
+    return true
 };
