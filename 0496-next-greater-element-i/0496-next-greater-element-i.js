981a25ecddc7a90ef71eb6a727c85741daf1b37f
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-14 19:34:56 +0530

    Time: 4 ms (62.28%), Space: 52.6 MB (32.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,24 +4,29 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-  let result = []
-
-  for(let i = 0; i < nums1.length; i++) {
-    const num = nums1[i]
-        let found = false
-        let ng = - 1
-
-    for(let j = 0; j < nums2.length; j++) {
-        if(nums2[j] === num) {
-            found = true
-        }
-
-        if(found && nums2[j] > num) {
-            ng = nums2[j]
-            break
-        }
+  let stack = []
+  let map = new Map()
+  
+  for(let i = nums2.length - 1; i >= 0; i--) {
+    while(stack.length > 0 && stack[stack.length - 1] <= nums2[i]) {
+      stack.pop()
     }
-    result.push(ng)
-  }  
+    
+    if(stack.length === 0) {
+      map.set(nums2[i], -1)
+    } else {
+      map.set(nums2[i], stack[stack.length - 1])
+    }
+    stack.push(nums2[i])
+  }
+  
+  
+  let result = []
+  
+  for(let i = 0; i < nums1.length; i++) {
+    if(map.has(nums1[i])) {
+      result.push(map.get(nums1[i]))
+    }
+  }
   return result
 };
