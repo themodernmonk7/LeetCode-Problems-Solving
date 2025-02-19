bc0cb11ec688aa0ca3efd82793e22cae97179f46
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-19 20:30:11 +0530

    Time: 36 ms (99.39%), Space: 52.9 MB (10.85%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,6 +4,10 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
+    if(!Array.isArray(arr)) {
+        throw new TypeError(`${arr} is not an array`)
+    }
+
     let result = []
     for(let i = 0; i < arr.length; i++) {
         result.push(fn(arr[i], i, arr))
