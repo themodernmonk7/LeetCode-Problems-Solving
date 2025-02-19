99122bead324a2b4c80c0a25bfa4e4e61d6d4b6a
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-19 20:31:47 +0530

    Time: 46 ms (89.81%), Space: 53.8 MB (7.49%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -4,10 +4,14 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
+    if(!Array.isArray(arr)) {
+        throw new TypeError(`${arr} is not an array`)
+    }
+
     let result = []
 
     for(let i = 0; i < arr.length; i++) {
-        if(fn(arr[i], i)) {
+        if(fn(arr[i], i, arr)) {
             result.push(arr[i])
         }
     }
