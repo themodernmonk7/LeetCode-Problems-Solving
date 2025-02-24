9cde0bb3bc9328d08492def7c92461ee9ede988d
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-24 21:30:52 +0530

    Time: 1 ms (61.38%), Space: 54.2 MB (24.87%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+   let left = 0, right = height.length - 1;
+    let leftMax = 0, rightMax = 0;
+    let result = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                result += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                result += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+    return result;
+};
