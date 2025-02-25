da1327f5c4acae60e17c3ae0bc8d9c7aa975b266
Kumar Avishek (SAurav) <sauravavishek@gmail.com>
2025-02-25 23:15:35 +0530

    Time: 98 ms (88.01%), Space: 107.2 MB (36.55%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.capacity = capacity
+    this.cache = new Map()
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+  if(this.cache.has(key)) {
+    const value = this.cache.get(key)
+    this.cache.delete(key)
+    this.cache.set(key, value)
+    return value
+  } else {
+    return -1
+  }
+}
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+  if(this.cache.has(key)) {
+    this.cache.delete(key)
+  }
+  this.cache.set(key, value)
+  if (this.cache.size > this.capacity) {
+    this.cache.delete(this.cache.keys().next().value); // Remove LRU item
+  }
+  
+}
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
