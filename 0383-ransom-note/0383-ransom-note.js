a8d8350485c7b11f9d1853172993d6bbbd1b58c2
Kumar Avishek <sauravavishek@gmail.com>
2025-03-13 19:18:35 +0530

    Time: 24 ms (24.88%), Space: 60.5 MB (8.91%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    const charFreq = {}
+
+    for(let char of magazine) {
+        charFreq[char] = charFreq[char] ? charFreq[char] + 1 : 1
+    }
+
+
+    for(let char of ransomNote) {
+        if(!charFreq[char]) return false
+        charFreq[char]--
+    }
+
+    return true
+};
