a663cf6e87c9bc81c957b697d5602003590615bd
Kumar Avishek <sauravavishek@gmail.com>
2025-03-17 23:33:26 +0530

    Time: 36 ms (78.13%), Space: 66.6 MB (88.4%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -4,22 +4,20 @@
  */
 var longestConsecutive = function(nums) {
     if(nums.length === 0) return 0
+    const n = nums.length
     const sortNums = nums.sort((a, b) => a - b)
-    const n = sortNums.length
-
-    let maxLength = 1
     let streak = 1
+    let maxLength = 1
 
-    for(let i = 1; i < n; i++) {
-        if(nums[i] === nums[i - 1]) continue
+    for(let i = 1; i < nums.length; i++ ) {
+        if(sortNums[i] === sortNums[i - 1]) continue
 
-        if(nums[i] === nums[i - 1] + 1) {
+        if(sortNums[i] === sortNums[i - 1] + 1) {
             streak++
         } else {
             maxLength = Math.max(maxLength, streak)
             streak = 1
         }
     }
-
     return Math.max(maxLength, streak)
 };
