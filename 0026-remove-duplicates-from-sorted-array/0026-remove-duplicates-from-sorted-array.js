2a35e097e16c00fdc53f40c373bfabc065bce369
Kumar Avishek <sauravavishek@gmail.com>
2025-03-23 22:32:06 +0530

    Time: 0 ms (100%), Space: 58.2 MB (16.49%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -2,15 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+ /**
+ - Sorted array, which means that duplicates element will be beside each other
+ - If the current element is equal to the next element then skip that element using continue
+ - If the current element is not equal to the next element, then same the particular element in the array
+ - Return the count of unique elements after removing the duplicates
+  */
+
 var removeDuplicates = function(nums) {
     const n = nums.length
-    let k = 0
+    let index = 0
 
     for(let i = 1; i < n; i++) {
-        if(nums[i] !== nums[k]) {
-            k++
-            nums[k] = nums[i]
+        if(nums[i] !== nums[index]) {
+            index++
+            nums[index] = nums[i]
         }
     }
-    return k + 1
+    return index + 1
 };
