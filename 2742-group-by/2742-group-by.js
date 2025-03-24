b4fdac7fe45e8333fc592f43a1f83706f92e35b6
Kumar Avishek <sauravavishek@gmail.com>
2025-03-24 19:48:01 +0530

    Time: 84 ms (98.96%), Space: 79.1 MB (17.64%) - LeetHub

--- 2742-group-by/2742-group-by.js
+++ 2742-group-by/2742-group-by.js
@@ -4,14 +4,14 @@
  */
 Array.prototype.groupBy = function(fn) {
     
-    return this.reduce((result, value) => {
-        const key = fn(value)
-        if(!result[key]) {
-            result[key] = []
-        }
-        result[key].push(value)
-        return result
-    }, {})
+  return this.reduce((result, value) => {
+    const key = fn(value)
+    if(!result[key]) {
+        result[key] = []
+    }
+    result[key].push(value)
+    return result
+  }, {})
 };
 
 /**
