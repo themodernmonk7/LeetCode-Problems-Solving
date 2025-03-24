2a72cc6c026d06b977cf5bf2483738925a5cbc67
Kumar Avishek <sauravavishek@gmail.com>
2025-03-24 09:46:04 +0530

    Time: 8 ms (89.76%), Space: 57 MB (40.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,16 +5,18 @@
  */
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false
-
     let charFreq = {}
 
-    for(let char of s) {
+    for(let i = 0; i < s.length; i++) {
+        const char = s[i]
         charFreq[char] = charFreq[char] ? charFreq[char] + 1 : 1
     }
 
-    for(let char of t) {
+    for(let i = 0; i < t.length; i++) {
+        const char = t[i]
         if(!charFreq[char]) return false
         charFreq[char]--
     }
+
     return true
 };
