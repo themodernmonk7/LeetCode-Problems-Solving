efe35519ad183cd0cc00794a65fad4febe8932c9
Kumar Avishek <sauravavishek@gmail.com>
2025-03-24 19:30:49 +0530

    Time: 258 ms (78.93%), Space: 95.7 MB (20.85%) - LeetHub

--- 2731-memoize/2731-memoize.js
+++ 2731-memoize/2731-memoize.js
@@ -6,9 +6,11 @@ function memoize(fn) {
     let cache = new Map()
     return function(...args) {
         const key = JSON.stringify(args)
+
         if(cache.has(key)) {
             return cache.get(key)
         }
+
         const result = fn(...args)
         cache.set(key, result)
         return result
