63184451ab350e981d4dbe1f1f9788f0d8481f86
Kumar Avishek <sauravavishek@gmail.com>
2025-03-24 19:38:02 +0530

    Time: 55 ms (19.58%), Space: 53.7 MB (36.87%) - LeetHub

--- 2864-is-object-empty/2864-is-object-empty.js
+++ 2864-is-object-empty/2864-is-object-empty.js
@@ -3,10 +3,8 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    if(typeof obj !== 'object' || obj === null) {
+    if(typeof obj !== 'object' || obj === null)  {
         throw new TypeError(`${obj} is not an object`)
     }
-
-    if(Object.keys(obj).length === 0 ) return true
-    return false
+    return Object.keys(obj).length === 0  
 };
