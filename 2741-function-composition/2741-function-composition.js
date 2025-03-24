440f6c21567354c349bec62a6d793e626d255a2d
Kumar Avishek <sauravavishek@gmail.com>
2025-03-24 19:27:05 +0530

    Time: 43 ms (97.22%), Space: 54.1 MB (49.44%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -5,7 +5,7 @@
 var compose = function(functions) {
     
     return function(x) {
-        return functions.reduceRight((acc, fn) => fn(acc), x);
+        return functions.reduceRight((acc, fn) => fn(acc), x)   
     }
 };
 
