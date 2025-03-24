a54e41439344ac9863d18f58c8bfd1da92ab4897
Kumar Avishek <sauravavishek@gmail.com>
2025-03-24 23:08:04 +0530

    Time: 6 ms (40.86%), Space: 59.2 MB (8.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
+    const n = nums.length
     let freq = {}
-
     for(let num of nums) {
-        freq[num] = freq[num] ? freq[num] + 1 : 1
-        if(freq[num] > nums.length/2) {
-        return num
-        } 
+        freq[num] = freq[num]  ? freq[num] + 1 : 1
+
+        if(freq[num] > n / 2) return num
     }
 };
