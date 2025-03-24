d220613b10e282ac3a88556e6259953e6370026f
Kumar Avishek <sauravavishek@gmail.com>
2025-03-24 22:05:33 +0530

    Time: 3 ms (71.88%), Space: 57.2 MB (54.31%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -4,19 +4,20 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-  const n = nums.length
-  let left = 0
-  let sum = 0
-  let minLength = Infinity
+    const n = nums.length
+    let left = 0
+    let sum = 0
+    let minSize = Infinity
 
-  for(let i = 0; i < n; i++) {
-    sum += nums[i]
-    while(sum >= target) {
-        minLength = Math.min(minLength, i - left + 1)
-        sum -= nums[left]
-        left++
+    for(let i = 0; i < n; i++) {
+        sum += nums[i]
+
+        while(sum >= target) {
+            minSize = Math.min(minSize, i - left + 1)
+            sum -= nums[left]
+            left++
+        }
     }
-  }
 
-  return minLength === Infinity ? 0 : minLength
+    return minSize === Infinity ? 0 : minSize
 };
