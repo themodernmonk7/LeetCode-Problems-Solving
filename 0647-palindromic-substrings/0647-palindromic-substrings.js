18d497807653f2c5ccdeb961d9b1bb5a520b958d
Kumar Avishek <sauravavishek@gmail.com>
2025-03-24 11:50:24 +0530

    Time: 9 ms (85.78%), Space: 56.7 MB (28.63%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.js
+++ 0647-palindromic-substrings/0647-palindromic-substrings.js
@@ -3,27 +3,28 @@
  * @return {number}
  */
 
- const countPalindromes = (string, left, right) => {
-    let count  = 0
-    while(left >= 0 && right < string.length && string[left] === string[right]) {
-        count++
-        left--
-        right++
-    }
-    return count
- }
+const countPalindromes = (s, left, right) => {
+  
+  let count = 0
+  
+  while(left >= 0 && right < s.length && s[left] === s[right]) {
+    count++
+    left--
+    right++
+  }
+  
+  return count
+}
 var countSubstrings = function(s) {
+  
     let count = 0
-
-    for(let i = 0; i < s.length; i++) {
-
+  for(let i = 0; i < s.length; i++) {
+    
     // count palindromes with odd length
     count += countPalindromes(s, i, i)
-
-    // count palindromes with event length
-    count += countPalindromes(s, i, i+1)
-
-    }
-    
-    return count
+    // count palindromes with even length
+    count += countPalindromes(s, i, i + 1)
+  }
+  
+  return count
 };
