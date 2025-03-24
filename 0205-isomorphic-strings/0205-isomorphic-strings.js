0d060bdc89d0fb30b239945e9c045fa4df6b4033
Kumar Avishek <sauravavishek@gmail.com>
2025-03-24 22:12:17 +0530

    Time: 12 ms (14.39%), Space: 56.4 MB (15.47%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,27 +4,30 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
+
     if(s.length !== t.length) return false
 
-    let sMap = {}
-    let tMap = {}
+    let sMap = new Map()
+    let tMap = new Map()
+
 
     for(let i = 0; i < s.length; i++) {
         const char = s[i]
-        if(!sMap[char]) {
-            sMap[char] = t[i]
+        if(!sMap.has(char)) {
+            sMap.set(char, t[i] )
         }
 
-        if(sMap[char] && sMap[char] !== t[i]) return false
+        if(sMap.has(char) && sMap.get(char) !== t[i]) return false
     }
 
     for(let i = 0; i < t.length; i++) {
         const char = t[i]
-        if(!tMap[char]) {
-            tMap[char] = s[i]
+        if(!tMap.has(char)) {
+            tMap.set(char, s[i])
         }
-
-        if(tMap[char] && tMap[char] !== s[i]) return false
+        if(tMap.has(char) && tMap.get(char) !== s[i]) return false
     }
+
+
     return true
 };
