61c40d7dea3f7341932e769c8f8c6f2c1bc54e63
Kumar Avishek <sauravavishek@gmail.com>
2025-03-27 22:45:50 +0530

    Time: 101 ms (16.64%), Space: 87.2 MB (11.03%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -5,14 +5,15 @@
 var eraseOverlapIntervals = function(intervals) {
     intervals.sort((a, b ) => a[1] - b[1])
     
-    let start = intervals[0][1]
+    let end = intervals[0][1]
     let count = 0
+    console.log(intervals)
     
     for(let i = 1; i < intervals.length; i++) {
-      if(intervals[i][0] < start) {
+      if(intervals[i][0] < end) {
         count++
       } else {
-        start = intervals[i][1]
+        end = intervals[i][1]
       }
     }
     return count
