f866c6b6123ae93ce4acb2799cf0f4234459115d
Kumar Avishek <sauravavishek@gmail.com>
2025-03-27 19:03:16 +0530

    Time: 0 ms (100%), Space: 53.9 MB (14.96%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -2,6 +2,17 @@
  * @param {string} s
  * @return {string}
  */
+
+  /**
+  * Iterate through the string
+  * Create two stack: 1. for number and 2. for character
+  * When you find a number add that number in the number stack
+  * If you find a character or opening bracket, add it into the character stack
+  * When you find a closing bracket, start popping till the opening bracket
+  * and multiply the string with the top number from the number stack
+  * push the multiplied string into the stack
+  */
+  
 var decodeString = function(s) {
     let numStack = [], charStack = []
     let currentNum = 0, currentString = ''
