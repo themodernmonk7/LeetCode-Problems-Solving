a5b30dcc4124f492f04a3aa577446fe91d0a7a51
Kumar Avishek <sauravavishek@gmail.com>
2025-03-27 22:32:12 +0530

    Time: 83 ms (66.46%), Space: 79.5 MB (29.28%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -2,6 +2,13 @@
  * @param {number[][]} points
  * @return {number}
  */
+
+  /**
+  * Start with the first balloon’s end (x_end) as the first arrow position.
+    If current balloon's start (x_start) ≤ previous arrow's end (x_end), then they overlap.
+    If the next balloon overlaps, do nothing (since one arrow can burst both).
+    If it doesn’t overlap, increase the arrow count and move to the next balloon’s end.
+  */
 var findMinArrowShots = function(points) {
     let sortPoints = points.sort((a, b) => a[1] - b[1])
     let end = sortPoints[0][1]
