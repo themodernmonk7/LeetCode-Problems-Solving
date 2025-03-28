3be01a7460c5c35def892149d57dc38df0229b4e
Kumar Avishek <sauravavishek@gmail.com>
2025-03-28 11:52:51 +0530

    Time: 3 ms (46.19%), Space: 58.3 MB (22.88%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    const n = nums.length
+    let sumLeft = 0
+    let sumRight = nums.reduce((acc, curr) => acc + curr, 0)
+
+    for(let i = 0; i < n; i++) {
+        sumRight -= nums[i]
+
+        if(sumLeft === sumRight) return i
+
+        sumLeft += nums[i]
+    }
+
+    return -1
+};
