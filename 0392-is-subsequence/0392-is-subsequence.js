359831f2071f78d2cb1b947da8f93a8522832a1f
Kumar Avishek <sauravavishek@gmail.com>
2025-05-24 21:22:35 +0530

    Time: 1 ms (72.66%), Space: 54.3 MB (85.25%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -6,11 +6,11 @@
 var isSubsequence = function(s, t) {
   let i = 0
   let j = 0
-
-  while(i < s.length && j < t.length) {
-    if(s[i] === t[j]) i++
+  
+  while (i < s.length && j < t.length) {
+    if(s[i] == t[j]) i++
     j++
-  }  
-
+  }
+  
   return i === s.length
 };
