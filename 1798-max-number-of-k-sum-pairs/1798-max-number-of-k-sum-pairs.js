4345621ad3857acb4f87484e28a65b8b96fc0163
Kumar Avishek <sauravavishek@gmail.com>
2025-05-25 09:29:44 +0530

    Time: 69 ms (97.61%), Space: 69.9 MB (31.85%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.js
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.js
@@ -4,19 +4,22 @@
  * @return {number}
  */
 var maxOperations = function(nums, k) {
-    let map = new Map()
-    let count = 0
-
-    for(let num of nums) {
-        let secondNum = k - num
-
-        if(map.has(secondNum) && map.get(secondNum) > 0) {
-            count++
-            map.set(secondNum, map.get(secondNum) - 1)
-        } else {
-            map.set(num, (map.get(num) || 0) + 1 )
-        }
+  const n = nums.length
+  let operations = 0
+  let map = new Map()
+  
+  
+  for(let i = 0; i < n; i++) {
+    const currentNum = nums[i]
+    const secondNum = k - currentNum
+    
+    if(map.has(secondNum) && map.get(secondNum) > 0) {
+      operations++
+      map.set(secondNum, map.get(secondNum) - 1)
+    } else {
+      map.set(currentNum, (map.get(currentNum) || 0) + 1 )
     }
-
-    return count
+  }
+  
+  return operations
 };
