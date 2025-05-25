6191a1636d18d96710f84eb45da7d36b9b926cd2
Kumar Avishek <sauravavishek@gmail.com>
2025-05-25 10:32:03 +0530

    Time: 19 ms (50.06%), Space: 56.7 MB (56.23%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -4,22 +4,27 @@
  * @return {number}
  */
 var maxVowels = function(s, k) {
-    const n = s.length
-    let vowels = new Set(["a", "e", "i", "o", "u"] )
-    let maxVowels = 0, currentVowels = 0
-
-    for(let i = 0; i < k; i++) {
-        if(vowels.has(s[i])) {
-            currentVowels++
-        }
+  const n = s.length
+  let vowels = new Set(["a", "e", "i", "o", "u"] )
+  
+  let maxVowels = 0
+  let currentVowelsCount = 0
+  
+  for(let i = 0; i < k; i++) {
+    const currentCharacter = s[i]
+    if(vowels.has(currentCharacter)) {
+      currentVowelsCount++
     }
-
-    maxVowels = currentVowels
-
-    for(let i = k; i < n; i++) {
-        if(vowels.has(s[i])) currentVowels++
-        if(vowels.has(s[i - k])) currentVowels--
-        maxVowels = Math.max(maxVowels, currentVowels)
-    }
-    return maxVowels
+  }
+  
+  maxVowels = currentVowelsCount
+  
+  for(let i = k; i < n; i++) {
+    
+    if(vowels.has(s[i])) currentVowelsCount++
+    if(vowels.has(s[i - k])) currentVowelsCount--
+    maxVowels = Math.max(maxVowels, currentVowelsCount)
+  }
+  
+  return maxVowels
 };
