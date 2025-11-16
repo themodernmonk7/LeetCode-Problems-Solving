033987a0c00dc0721515a35b2025ade5f7b16c5f
Kumar Avishek <sauravavishek@gmail.com>
2025-11-16 21:48:12 +0530

    Time: 59 ms (47.4%), Space: 59.8 MB (28.07%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -13,7 +13,8 @@
 var hasCycle = function(head) {
     let slow = head
     let fast = head
-    while(fast !== null && fast.next !== null) {
+
+    while(fast && fast.next) {
         slow = slow.next
         fast = fast.next.next
         if(slow === fast) return true
