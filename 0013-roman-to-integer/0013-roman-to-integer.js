60f58f0e62da71af0f4f3de5bf2086ccf3f8c88e
Kumar Avishek <sauravavishek@gmail.com>
2026-01-18 10:45:33 +0530

    Time: 5 ms (53.14%), Space: 60.9 MB (51.09%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -12,17 +12,19 @@ var romanToInt = function(s) {
             D:500,
             M:1000,
     }
-
+    
     const n = s.length
+    
     let result = 0
-
+    
+    
     for(let i = 0; i < n; i++) {
-        if(i < n - 1 && numStore[s[i]] < numStore[s[i + 1]]) {
-            result -= numStore[s[i]]
-        } else {
-            result += numStore[s[i]]
-        }
+      if(i < n && numStore[s[i]] < numStore[s[i + 1]] ) {
+        result -= numStore[s[i]]
+      } else {
+        result += numStore[s[i]]
+      }
     }
-
+    
     return result
 };
