4d5aced759af51e96347d5eaeb877f7e3f72ca05
Kumar Avishek <sauravavishek@gmail.com>
2026-01-18 11:01:48 +0530

    Time: 7 ms (36.07%), Space: 62.2 MB (15.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,8 +2,46 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    const cleanString = s.replace(/[^a-z0-9]/gi,"").toLowerCase()
+
+const isAlphanumeric = (string) => {
+  
+  if(typeof string !== 'string') {
+    throw new TypeError(`${string} must be a string`)
+  }
+  const value = string.trim()
+  const regex = /^[a-z0-9]+$/i
+  const isValid = regex.test(value)
+  
+  if(isValid) {
+    return true
     
-    return cleanString.split("").reverse().join("") === cleanString
+  } else {
+    return false
+  }
+}
+ 
+var isPalindrome = function(string) {
+      const n = string.length
+  let left = 0
+  let right = n - 1
+  
+  while(left < right) {
+    
+    if(!isAlphanumeric(string[left])) {
+      left++
+      continue
+    }
+    
+    if(!isAlphanumeric(string[right])) {
+      right--
+      continue
+    }
+    
+    if(string[left].toLowerCase() !== string[right].toLowerCase()) return false
+    left++
+    right--
+  }
+  
+  return true
+
 };
