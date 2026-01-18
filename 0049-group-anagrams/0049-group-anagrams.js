557a659592209e65153fec023f5df09459ece2a4
Kumar Avishek <sauravavishek@gmail.com>
2026-01-18 11:36:28 +0530

    Time: 33 ms (56.35%), Space: 67.4 MB (72.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -7,11 +7,14 @@ var groupAnagrams = function(strs) {
     let map = new Map()
 
     for(let i = 0; i < n; i++) {
-        const sortString = strs[i].split('').sort().join("")
+        const sortString = strs[i].split("").sort().join("")
+
         if(!map.has(sortString)) {
             map.set(sortString, [])
         }
+
         map.get(sortString).push(strs[i])
     }
+
     return Array.from(map.values())
 };
