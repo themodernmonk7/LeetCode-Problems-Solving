02f27ecef26e2a45135391807b4258dee1bd9a90
Kumar Avishek <sauravavishek@gmail.com>
2026-01-25 21:25:39 +0530

    Time: 1 ms (95.43%), Space: 65.7 MB (37.98%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,18 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let maxProfit = 0
-  let bestBuyStock = prices[0]
-  const n = prices.length
-
-  for(let i = 1; i < n; i++) {
+ let max = 0
+  let bestBuy = prices[0]
+  
+  for(let i = 1; i < prices.length; i++) {
     const price = prices[i]
-    if(price > bestBuyStock) {
-        maxProfit = Math.max(maxProfit, (price - bestBuyStock))
+    
+    if(price > bestBuy) {
+      max = Math.max(max, (price - bestBuy))
+    } else {
+      bestBuy = Math.min(bestBuy, price)
     }
-    bestBuyStock = Math.min(bestBuyStock, price)
   }
-  return maxProfit
+  return max
+
 };
