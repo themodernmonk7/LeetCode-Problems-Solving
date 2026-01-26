0870da60949ba643c232890a1766c3d23bab6bb5
Kumar Avishek <sauravavishek@gmail.com>
2026-01-26 19:23:23 +0530

    Time: 48 ms (35.42%), Space: 53.5 MB (78.56%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,12 +4,18 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    if(!Array.isArray(arr)) throw new TypeError(`${arr} must be an array`)
-    if(typeof fn !== 'function') throw new TypeError(`${fn} must be a function`)
+    if(!Array.isArray) {
+        throw new TypeError(`${arr} must be an array`) 
+    }
 
+    if(typeof fn !== 'function') {
+        throw new TypeError(`${fn} must ben an array`)
+    }
+
+    const n = arr.length
     let result = []
 
-    for(let i = 0; i < arr.length; i++) {
+    for(let i = 0; i < n; i++) {
         if(i in arr) {
             if(fn(arr[i], i)) {
                 result.push(arr[i])
