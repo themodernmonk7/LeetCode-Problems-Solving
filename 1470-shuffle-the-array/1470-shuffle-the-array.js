8e7260efb8424d5736c1c31a187ce88a7a499e43
Kumar Avishek <sauravavishek@gmail.com>
2026-01-28 21:57:52 +0530

    Time: 63 ms (9.79%), Space: 58 MB (39.31%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -5,9 +5,10 @@
  */
 var shuffle = function(nums, n) {
     let result = []
-    for(let i= 0; i < n; i++) {
-        result.push(nums[i], nums[i+n])
+
+    for(let i = 0; i < n; i++) {
+        result.push(nums[i], nums[i + n])
     }
+
     return result
 };
-
