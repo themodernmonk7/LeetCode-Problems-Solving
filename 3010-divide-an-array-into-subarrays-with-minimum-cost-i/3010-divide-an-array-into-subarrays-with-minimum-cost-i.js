24320bb1df2133622305cb96b006a98d76402e06
Kumar Avishek <sauravavishek@gmail.com>
2026-02-01 23:27:02 +0530

    Time: 0 ms (100%), Space: 57.4 MB (23.08%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.js
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.js
@@ -3,6 +3,18 @@
  * @return {number}
  */
 var minimumCost = function(nums) {
-    const result = nums.slice(1).sort((a, b) => a - b)
-    return nums[0] + result[0] + result[1]
+    const n = nums.length
+    let min1 = Infinity
+    let min2 = Infinity
+
+    for(let i = 1; i < n; i++) {
+        if(nums[i] < min1) {
+            min2 = min1
+            min1 = nums[i]
+        } else if (nums[i] < min2) {
+            min2 = nums[i]
+        }
+    }
+
+    return nums[0] + min1 + min2
 };
