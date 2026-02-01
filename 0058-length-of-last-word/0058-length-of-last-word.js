21302c6b62b3ac0e36631b530ebdd40effba02b1
Kumar Avishek <sauravavishek@gmail.com>
2026-02-01 23:36:16 +0530

    Time: 0 ms (100%), Space: 53.9 MB (51.89%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    const n = s.length
+    let i = n - 1
+    let length = 0
+
+    while(i >= 0 && s[i] === ' ') {
+        i--
+    }
+
+    while(i >= 0 && s[i] !== ' ') {
+        length++
+        i--
+    }
+
+    return length
+};
