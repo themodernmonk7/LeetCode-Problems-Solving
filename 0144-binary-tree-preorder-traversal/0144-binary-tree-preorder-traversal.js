a6c286fb38fa92de42fc9176d6eb2544906edc33
Kumar Avishek <sauravavishek@gmail.com>
2026-02-08 20:03:41 +0530

    Time: 0 ms (100%), Space: 54 MB (51.14%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -11,17 +11,17 @@
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
-    
-    const output = []
+    const result = []
 
-    function preOrder (root) {
+    const preOrder = (root) => {
         if(root) {
-            output.push(root.val)
+            result.push(root.val)
             preOrder(root.left)
             preOrder(root.right)
         }
     }
 
+    
     preOrder(root)
-    return output
+    return result
 };
