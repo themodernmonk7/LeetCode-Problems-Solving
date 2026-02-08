7338405327bb92a8f20a3ce6281b8aa876f3a4d0
Kumar Avishek <sauravavishek@gmail.com>
2026-02-08 18:13:01 +0530

    Time: 0 ms (100%), Space: 53.8 MB (52.4%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -2,36 +2,29 @@
  * @param {string} s
  * @return {string}
  */
-
-  /**
-  * Iterate through the string
-  * Create two stack: 1. for number and 2. for character
-  * When you find a number add that number in the number stack
-  * If you find a character or opening bracket, add it into the character stack
-  * When you find a closing bracket, start popping till the opening bracket
-  * and multiply the string with the top number from the number stack
-  * push the multiplied string into the stack
-  */
-  
 var decodeString = function(s) {
-    let numStack = [], charStack = []
-    let currentNum = 0, currentString = ''
-
-    for(let char of s) {
-        if(!isNaN(char)) {
-            currentNum = currentNum * 10 + Number(char)
-        } else if(char === '[') {
-            numStack.push(currentNum)
-            charStack.push(currentString)
-            currentString = ''
-            currentNum = 0
-        } else if(char === ']') {
-            let repeatTimes = numStack.pop()
-            let lastString = charStack.pop()
-            currentString = lastString + currentString.repeat(repeatTimes)
-        } else {
-            currentString += char
-        }
+      let numStack = [], charStack = []
+  let currString = '', currNum = 0
+  
+  
+  for(let char of s) {
+    
+    if(!isNaN(char)) {
+        // this is important to handle to double digit number
+      currNum = currNum * 10 + Number(char)
+    } else if (char === '[') {
+      numStack.push(currNum)
+      charStack.push(currString)
+      currString = ''
+      currNum = 0
+    } else if (char === ']') {
+      let repeatTimes = numStack.pop()
+      let lastString = charStack.pop()
+      currString = lastString + currString.repeat(repeatTimes)
+    } else {
+      currString += char
     }
-    return currentString
+  }
+  
+  return currString
 };
