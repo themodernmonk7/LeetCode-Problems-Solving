31780d65ae2f2aa8a4eb390fdfe5b938da2b7964
Kumar Avishek <sauravavishek@gmail.com>
2026-02-08 20:16:42 +0530

    Time: 0 ms (100%), Space: 59 MB (15%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,10 +11,10 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (root === null) return 0;
+    if(root === null) return 0
 
-  const leftDepth = maxDepth(root.left);
-  const rightDepth = maxDepth(root.right);
+    const leftDepth = maxDepth(root.left)
+    const rightDepth = maxDepth(root.right)
 
-  return Math.max(leftDepth, rightDepth) + 1;
+    return Math.max(leftDepth, rightDepth) + 1
 };
