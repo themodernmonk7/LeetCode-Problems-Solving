e6b3c8cb3871606dd014b17e0c014460a4d47a82
Kumar Avishek <sauravavishek@gmail.com>
2026-02-11 22:31:36 +0530

    Time: 0 ms (100%), Space: 53.7 MB (63.2%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -5,8 +5,9 @@
  */
 var removeElement = function(nums, val) {
     let index = 0
+    const n = nums.length
 
-    for(let i = 0; i < nums.length; i++) {
+    for(let i = 0; i < n; i++) {
         if(nums[i] !== val) {
             nums[index] = nums[i]
             index++
