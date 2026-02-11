d1f25746c0e8a3fe11821b5b2e6a9fb202d181c6
Kumar Avishek <sauravavishek@gmail.com>
2026-02-11 21:02:31 +0530

    Time: 10 ms (81.62%), Space: 60 MB (23.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,26 +4,23 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    const sN = s.length
-    const tN = t.length
-    
-    if(sN !== tN) return false
-    
-   const charFreq = {}
-   
-   for(let i = 0; i < sN; i++) {
-     const char = s[i]
-     
-     charFreq[char] = charFreq[char] ? charFreq[char] + 1 : 1
-   }
-   
-   
-   for(let i = 0; i < tN; i++) {
-     const char = t[i]
-     if(!charFreq[char]) return false
-     charFreq[char]--
-   }
-   
-   return true
+  let n1 = s.length
+  let n2 = t.length
+  
+  if(n1 !== n2) return false
+  
+  const charFreq = {}
+  
+  for(let char of s) {
+    charFreq[char] = charFreq[char] ? charFreq[char] + 1 : 1
+  }
+  
+  
+  for(let char of t) {
+    if(!charFreq[char]) return false
+    charFreq[char]--
+  }
+  
+  return true
     
 };
