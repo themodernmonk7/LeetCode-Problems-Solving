bcef3952c2cfca931ccaf3ebb3328036948c993c
Kumar Avishek <sauravavishek@gmail.com>
2026-04-24 08:48:07 +0530

    Time: 44 ms (5.16%), Space: 53.4 MB (66.67%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -4,21 +4,22 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, s) {
-    const words = s.split(' ')
-
-    if(pattern.length !== words.length) return false
-
-    let pMap = new Map()
+const pLength = pattern.length
+    const sWord = s.split(' ')
+    const sLength = sWord.length
+    
+    if(pLength !== sLength) return false
+    
+    let map = new Map()
     let sMap = new Map()
-
-    for(let i = 0; i < pattern.length; i++) {
-        let pChar = pattern[i], wChar = words[i]
-
-        if(pMap.has(pChar) && pMap.get(pChar) !== wChar || sMap.has(wChar) && sMap.get(wChar) !== pChar) return false
-
-        pMap.set(pChar, wChar)
-        sMap.set(wChar, pChar)
-
+    
+    for(let i = 0; i < pLength; i++) {
+      let pChar = pattern[i], sChar = sWord[i]
+      
+      if(map.has(pChar) && map.get(pChar) !== sChar || sMap.has(sChar) && sMap.get(sChar) !== pChar) return false
+      map.set(pChar, sChar)
+      sMap.set(sChar, pChar)
     }
+    
     return true
 };
