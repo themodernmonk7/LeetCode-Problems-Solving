4e1f50b23c7d592ca1e87d3143db3d3f8e7eadba
Kumar Avishek <sauravavishek@gmail.com>
2026-04-25 12:50:14 +0530

    Time: 6 ms (66.13%), Space: 58.7 MB (47.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -4,12 +4,14 @@
  */
 var lengthOfLongestSubstring = function(s) {
     const n = s.length
-    let set = new Set()
     let left = 0
     let maxLength = 0
+    let set = new Set()
+
 
     for(let i = 0; i < n; i++) {
         const char = s[i]
+
         while(set.has(char)) {
             set.delete(s[left])
             left++
@@ -17,5 +19,6 @@ var lengthOfLongestSubstring = function(s) {
         set.add(char)
         maxLength = Math.max(maxLength, i - left + 1)
     }
+
     return maxLength
 };
