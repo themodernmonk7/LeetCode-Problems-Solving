a5d6ede28b7fcbe7b0305c70ac577c4bbe409fa4
Kumar Avishek <sauravavishek@gmail.com>
2026-04-25 13:45:49 +0530

    Time: 2 ms (82.9%), Space: 68.8 MB (68.01%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -4,20 +4,24 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-  const n = nums.length
-  let currentSum = 0
-  let maxSum = 0
-  
-  for(let i = 0; i < k; i++) {
-    currentSum += nums[i]
-  }
-  
-  maxSum = currentSum
-  
-  for(let i = k; i < n; i++) {
-    currentSum += nums[i] - nums[i - k]
-    maxSum = Math.max(maxSum, currentSum)
-  }
-  
-  return maxSum / k
+    const n = nums.length
+    
+    let maxSum = 0
+    let currentSum = 0
+    let left = 0
+    
+    
+    for(let i = 0; i < k; i++) {
+      currentSum += nums[i]
+      
+    }
+    
+    maxSum = currentSum
+    
+    for(let i = k; i < n; i++) {
+      currentSum += nums[i] - nums[i - k]
+      maxSum = Math.max(maxSum, currentSum)
+    }
+    
+    return maxSum / k
 };
