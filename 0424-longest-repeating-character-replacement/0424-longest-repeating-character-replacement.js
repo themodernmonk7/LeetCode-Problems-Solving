62c9bd1c3378e1563be6802e5daa8c3aa1ee00ff
Kumar Avishek <sauravavishek@gmail.com>
2026-05-24 09:57:09 +0530

    Time: 23 ms (31.12%), Space: 57.1 MB (39.72%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,31 +4,27 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-  const n = s.length
-  let left = 0
-  let freqChar = {}
-  let maxSize = 0
-  let maxFreq = 0
-  
-  
-  for(let i = 0; i < n; i++) {
-    
-    const char = s[i]
-    // update the frequency of the character
-    freqChar[char] = freqChar[char] ? freqChar[char] + 1 : 1
-    // update the maximum frequencey of the character
-    maxFreq = Math.max(maxFreq, freqChar[char])
-    
-    // characters neet to be changed = window - maxFreq > k then we need to shrink the window
-    
-    if((i - left + 1) - maxFreq > k) {
-      freqChar[s[left]]--
-      left++
+    const n = s.length
+    let left = 0
+    let freqChar = {}
+    let maxSize = 0
+    let maxFreq = 0
+
+    for(let i = 0; i < n; i++) {
+        const char = s[i]
+        freqChar[char] = freqChar[char] ? freqChar[char] + 1 : 1
+        maxFreq = Math.max(maxFreq, freqChar[char])
+
+        // Charcter need to be change when windowSize - maxFreq > k
+        // reduce the window size
+        // const windowSize = i - left + 1
+        if((i - left + 1) - maxFreq > k) {
+            freqChar[s[left]]--
+            left++
+        }
+
+        maxSize = Math.max(maxSize, i - left + 1)
     }
-    
-    maxSize = Math.max(maxSize, i - left + 1 )
-    
-  }
-  
-  return maxSize
+
+    return maxSize
 };
