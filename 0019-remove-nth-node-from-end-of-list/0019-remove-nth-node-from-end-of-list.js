350b5b78b87886678974a3351dd108858390108b
Kumar Avishek <sauravavishek@gmail.com>
2026-05-24 14:59:20 +0530

    Time: 0 ms (100%), Space: 54.6 MB (46.64%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -14,18 +14,19 @@ var removeNthFromEnd = function(head, n) {
     let dummy = new ListNode(0)
     dummy.next = head
 
+
     let slow = dummy
     let fast = dummy
 
-    for(let i = 0; i <= n; i++) {
+    for(let i = 0; i <=n; i++) {
         fast = fast.next
     }
 
-    // Now move the both pointers one step at a timek until fast reaches the end
-    while(fast !== null) {
+    while(fast) {
         slow = slow.next
         fast = fast.next
     }
+
     slow.next = slow.next.next
     return dummy.next
 };
