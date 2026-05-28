c5b32db8749b97228f6c750f779f4cc0a7638c7b
Kumar Avishek <sauravavishek@gmail.com>
2026-05-28 07:47:18 +0530

    Time: 7 ms (9.7%), Space: 57.2 MB (9.41%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,17 +3,19 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+        
     const sortWords = strs.sort()
     const n = sortWords.length
-
+    
     let first = sortWords[0]
-    let last = sortWords[n - 1]
-    let result = ""
-
+    console.log(sortWords)
+    let last = sortWords[n-1]
+    
+    let result = ''
+    
     for(let i = 0; i < first.length; i++) {
-        if(first[i] !== last[i]) break
-        result += first[i]
+      if(first[i] !== last[i]) break
+      result += first[i]
     }
-
     return result
 };
