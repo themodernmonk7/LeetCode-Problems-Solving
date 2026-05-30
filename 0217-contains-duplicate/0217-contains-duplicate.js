01ad067901780402f34a12f4300be8c78dc9b155
Kumar Avishek <sauravavishek@gmail.com>
2026-05-30 22:43:26 +0530

    Time: 7 ms (99.01%), Space: 74.2 MB (47.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,14 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  const n = nums.length
+    const set = new Set()
 
-  const set = new Set()
+    for(let num of nums) {
+        if(set.has(num)) return true
+        set.add(num)
+    }
 
-  for(let num of nums) {
-    if(set.has(num)) return true
-    set.add(num)
-  }  
-
-  return false
+    return false
 };
