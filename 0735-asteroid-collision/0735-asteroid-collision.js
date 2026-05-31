5eb0263003e72982e24fedb4f12e0129f30e5f13
Kumar Avishek <sauravavishek@gmail.com>
2026-05-31 20:03:03 +0530

    Time: 8 ms (14.65%), Space: 57.4 MB (45.62%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.js
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -3,33 +3,31 @@
  * @return {number[]}
  */
 var asteroidCollision = function(asteroids) {
-  const stack = [];
+    let stack = []
 
-  for (let ast of asteroids) {
-    let alive = true;
 
-    while (
-      alive &&
-      stack.length > 0 &&
-      stack[stack.length - 1] > 0 &&
-      ast < 0
-    ) {
-      const top = stack[stack.length - 1];
+    for(let ast of asteroids) {
+        let alive = true
 
-      if (Math.abs(top) < Math.abs(ast)) {
-        stack.pop(); // top destroyed
-      } else if (Math.abs(top) > Math.abs(ast)) {
-        alive = false; // current destroyed
-      } else {
-        stack.pop();   // both destroyed
-        alive = false;
-      }
+        while(alive && stack.length > 0 && stack[stack.length - 1 ] > 0 && ast < 0) {
+
+            const top = stack[stack.length - 1]
+
+            if(Math.abs(top) < Math.abs(ast)) {
+                stack.pop()
+            } else if(Math.abs(top) > Math.abs(ast)) {
+                alive = false
+            } else {
+                stack.pop()
+                alive = false
+            }
+        }
+
+
+        if(alive) {
+            stack.push(ast)
+        }
     }
 
-    if (alive) {
-      stack.push(ast);
-    }
-  }
-
-  return stack;
+    return stack
 };
